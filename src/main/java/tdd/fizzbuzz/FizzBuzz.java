aff548c3a0b5476e093de3e5efe6d29bd7605678
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-05 11:40:48 +0800

    feat:implement countOff for multiple 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,8 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public String countOff(int number) {
+//        return number % 3 == 0 && number % 5 == 0 ? "FizzBuzz" : number % 3 ==0 ? "Fizz" : number % 5 == 0 ? "Buzz" : String.valueOf(number);
+        return number % 5 == 0 ? "Buzz" : null;
+     }
 }
