a743744def327ed78dd651a97b8881b96694d357
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-05 11:39:02 +0800

    test:add countOff multiple 5 test

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,19 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    private final String Buzz = "Buzz";
+    @Test
+    public void should_return_Buzz_when_countOff_multiple_5(){
+        //Given
+        int number = 5;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //When
+        String result = fizzBuzz.countOff(number);
+        //Then
+        assertEquals(Buzz,result);
+    }
 }
